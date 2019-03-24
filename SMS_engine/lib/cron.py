ec41fd8e7fdc567553432071a6ebb7d32529ee5b
shashi <shashi.kumar@carcrew.in>
2019-03-25 04:00:11 +0530

    engine removed

--- SMS_engine/lib/cron.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python
-from media_engine.lib.retry_sms import celery_repush
-celery_repush()
--- SMS_engine/lib/providers/aerialinkv4.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from media_engine.lib.providers.aerialink import Aerialink
-
-
-class Aerialinkv4(Aerialink):
-    pass
